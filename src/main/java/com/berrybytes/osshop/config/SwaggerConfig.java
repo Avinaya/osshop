397065890a5b58948e7e55550f3219f018a45241
aaavinaya <aacharya@inventechgroup.com>
2023-08-10 17:25:19 +0545

    swagger configuration added

---
+++ src/main/java/com/berrybytes/osshop/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.berrybytes.osshop.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private final static String API_VERSION = "1.0";
+    private final static String MODULE_NAME = "Osshop App";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title(MODULE_NAME).version(API_VERSION));
+    }
+}
